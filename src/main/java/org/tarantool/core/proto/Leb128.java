43b7f6ef20a248ff2de7e34224d9669bf2d9a3a7
Dmitry Grytsovets <dmitry.grytsovets@gmail.com>
2013-02-27 19:33:36 +0400

    Leb128 Fixed version for values more than 1 byte

--- src/main/java/org/tarantool/core/proto/Leb128.java
+++ src/main/java/org/tarantool/core/proto/Leb128.java
@@ -1,82 +1,84 @@
 package org.tarantool.core.proto;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
-/**
- * Leb128 encoder/decoder
- * 
- * @author dgreen
- * @version $Id: $
- */
+
 public class Leb128 {
+    /**
+     * <p>
+     * readUnsigned.
+     * </p>
+     *
+     * @param buffer
+     *            a {@link java.nio.ByteBuffer} object.
+     * @return a int.
+     */
+    public static int readUnsigned(ByteBuffer buffer) {
+        int result = 0;
+        int cur;
+        int count = 0;
+        do {
+            cur = buffer.get() & 0xff;
+            result <<=count * 7;
+            result |= (cur & 0x7f);
+            count++;
+        } while (((cur & 0x80) == 0x80));
 
-	/**
-	 * <p>
-	 * readUnsigned.
-	 * </p>
-	 * 
-	 * @param buffer
-	 *            a {@link java.nio.ByteBuffer} object.
-	 * @return a int.
-	 */
-	public static int readUnsigned(ByteBuffer buffer) {
-		int result = 0;
-		int cur;
-		int count = 0;
+        if ((cur & 0x80) == 0x80) {
+            throw new IllegalArgumentException("Can't read LEB128 from buffer");
+        }
 
-		do {
-			cur = buffer.get() & 0xff;
-			result |= (cur & 0x7f) << (count * 7);
-			count++;
-		} while (((cur & 0x80) == 0x80) && count < 5);
+        return result;
+    }
 
-		if ((cur & 0x80) == 0x80) {
-			throw new IllegalArgumentException("Can't read LEB128 from buffer");
-		}
+    /**
+     * <p>
+     * unsignedSize.
+     * </p>
+     *
+     * @param value
+     *            a int.
+     * @return a int.
+     */
+    public static int unsignedSize(int value) {
+        int remaining = value >> 7;
+        int count = 0;
+        while (remaining != 0) {
+            remaining >>= 7;
+            count++;
+        }
+        return count + 1;
+    }
 
-		return result;
-	}
+    /**
+     * <p>
+     * writeUnsigned.
+     * </p>
+     *
+     * @param buffer
+     *            a {@link java.nio.ByteBuffer} object.
+     * @param value
+     *            a int.
+     * @return a {@link java.nio.ByteBuffer} object.
+     */
+    public static ByteBuffer writeUnsigned(ByteBuffer buffer, int value) {
+        int remaining = value >>> 7;
+        int[] bts=new int[unsignedSize(value)];
+        int p=0;
+        while (remaining != 0) {
+            //buffer.put((byte) ((value & 0x7f) | 0x80));
+            bts[p++] = (value & 0x7f);
+            value = remaining;
+            remaining >>>= 7;
+        }
+        bts[p]=value;
+        for(int i=bts.length-1;i>0;i--) {
+            buffer.put((byte) ((bts[i] & 0x7f) | 0x80));
+        }
+        buffer.put((byte) (bts[0] & 0x7f));
+        return buffer;
+    }
 
-	/**
-	 * <p>
-	 * unsignedSize.
-	 * </p>
-	 * 
-	 * @param value
-	 *            a int.
-	 * @return a int.
-	 */
-	public static int unsignedSize(int value) {
-		int remaining = value >> 7;
-		int count = 0;
-		while (remaining != 0) {
-			remaining >>= 7;
-			count++;
-		}
-		return count + 1;
-	}
-
-	/**
-	 * <p>
-	 * writeUnsigned.
-	 * </p>
-	 * 
-	 * @param buffer
-	 *            a {@link java.nio.ByteBuffer} object.
-	 * @param value
-	 *            a int.
-	 * @return a {@link java.nio.ByteBuffer} object.
-	 */
-	public static ByteBuffer writeUnsigned(ByteBuffer buffer, int value) {
-		int remaining = value >>> 7;
-
-		while (remaining != 0) {
-			buffer.put((byte) ((value & 0x7f) | 0x80));
-			value = remaining;
-			remaining >>>= 7;
-		}
-
-		buffer.put((byte) (value & 0x7f));
-		return buffer;
-	}
+   
 }
